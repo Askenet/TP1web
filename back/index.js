b60dfb46abda837c368f2eba395392c3e70d319b
Askenet <101934186+Askenet@users.noreply.github.com>
2025-02-12 14:10:39 +0100

    ee

--- back/index.js
+++ back/index.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../front/TP1web")));
+app.use(express.static(path.join(__dirname, "../front")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../front/TP1web/index.html"));
+    res.sendFile(path.join(__dirname, "../front/index.html"));
     });
 PORT = process.env.PORT || 4001;
 
